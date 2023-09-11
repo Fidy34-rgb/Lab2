003429bb237a30174c50637413df0a00e933b350
Fidy <fidyantenainamanna.fiaferana@student.sl.on.ca>
2023-09-11 17:53:18 -0400

    Initial commit

---
+++ fidy/src/main/java/comp31/fidy/FidyApplication.java
@@ -0,0 +1,13 @@
+package comp31.fidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidyApplication.class, args);
+	}
+
+}
---
+++ fidy/src/test/java/comp31/fidy/FidyApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.fidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
