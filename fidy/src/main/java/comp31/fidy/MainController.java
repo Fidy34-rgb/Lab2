15c78f4a080587142d83c39a9d2371bb573a4106
Fidy <fidyantenainamanna.fiaferana@student.sl.on.ca>
2023-09-12 15:22:22 -0400

    Added image page

--- fidy/src/main/java/comp31/fidy/MainController.java
+++ fidy/src/main/java/comp31/fidy/MainController.java
@@ -8,9 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String getRoot() {
+        return "login";
+    }
+
     @GetMapping("/login")
     public String getLogin(@RequestParam String userId, Model model){
         model.addAttribute("userId", userId);
         return "home";
+    }
+
+    @GetMapping("/image")
+    public String getImage(@RequestParam String imageName, Model model){
+        model.addAttribute("name", imageName);
+        return "imagePage";
     }
 }
