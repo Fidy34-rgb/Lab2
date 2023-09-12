306915341dcc8885f31425fea9804ae1b883e5d0
Fidy <fidyantenainamanna.fiaferana@student.sl.on.ca>
2023-09-12 08:28:09 -0400

    Basic login complete

--- fidy/src/main/java/comp31/fidy/MainController.java
+++ fidy/src/main/java/comp31/fidy/MainController.java
@@ -1,14 +1,16 @@
 package comp31.fidy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin(@RequestParam String userId, Model model){
+        model.addAttribute("userId", userId);
         return "home";
     }
-    
 }
